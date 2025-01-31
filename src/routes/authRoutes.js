d18b9ddaf7721e2446cf92846ba1bca8fe84d43f
Amrut Patil <patilamrut578@gmail.com>
2025-01-31 21:52:12 +0530

    Interim commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { loginUser, logoutUser, registerUser } from '../controllers/authController.js';
+import User from '../models/User.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,8 +8,24 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
-router.get('/me', protect, (req, res) => {
-    res.json({ user: req.user });
+router.get("/me",protect,  async (req, res) => {
+    try {
+        const token = req.cookies.jwt;
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id).select("-password");
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        res.json({ user });
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
 });
 
 export default router;
