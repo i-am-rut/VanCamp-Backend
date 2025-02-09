80fedfbe94e9649ee644a9140e441aca4d0c0dca
Amrut Patil <patilamrut578@gmail.com>
2025-02-09 23:30:53 +0530

    Add express validator

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -4,20 +4,21 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// Route to check van availability
-router.get("/availability/:id", getVanAvailability);
-
 // Create a booking
 router.post('/create', protect, createBooking);
 
 // Get all bookings by a renter
 router.get('/mybookings', protect, getBookingsByRenter);
 
+// Get details of a specific booking
+router.get('/:id', protect, getBookingDetails);
+
+// Check van availability
+router.get("/availability/:id", getVanAvailability);
+
 // Cancel a booking
 router.patch('/cancel/:id', protect, cancelBooking);
 
-// Get details of a specific booking
-router.get('/:id', protect, getBookingDetails);
 
 
 
